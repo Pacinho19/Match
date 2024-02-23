5e033d646c3c02906047a8eb7d64d7353fdf6dad
patryk_ojdana <patryk.ojdana@axfina.pl>
2024-02-23 11:58:40 +0100

    Add test to BoardCube

---
+++ src/test/java/pl/pacinho/match/board/model/BoardCubeTest.java
@@ -0,0 +1,63 @@
+package pl.pacinho.match.board.model;
+
+import org.junit.jupiter.api.Test;
+import pl.pacinho.match.cube.model.Cube;
+import pl.pacinho.match.cube.model.CubeSide;
+import pl.pacinho.match.cube.model.CubeSideImage;
+import pl.pacinho.match.cube.model.CubeSideType;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class BoardCubeTest {
+
+    @Test
+    void exceptionShouldBeThrownWhenCubeSidesListNotContainingActiveSide() {
+        //given
+        Cube cube = new Cube(List.of(new CubeSide(CubeSideType.BACK, CubeSideImage.EVEREST)));
+        BoardCube boardCube = new BoardCube(cube, CubeSideType.FRONT);
+
+        //then
+        assertThrows(IllegalStateException.class, boardCube::getActiveCubeSideImage);
+    }
+
+    @Test
+    void correctCubeSideImageShouldBeReturnedFromGetActiveCubeSide() {
+        //given
+        Cube cube = new Cube(List.of(new CubeSide(CubeSideType.FRONT, CubeSideImage.EVEREST)));
+        BoardCube boardCube = new BoardCube(cube, CubeSideType.FRONT);
+
+        //when
+        CubeSideImage activeCubeSideImage = boardCube.getActiveCubeSideImage();
+
+        //then
+        assertThat(activeCubeSideImage, is(CubeSideImage.EVEREST));
+    }
+
+    @Test
+    void exceptionShouldBeThrownWhenCubeSidesListNotContainingGivenSide() {
+        //given
+        Cube cube = new Cube(List.of(new CubeSide(CubeSideType.BACK, CubeSideImage.EVEREST)));
+        BoardCube boardCube = new BoardCube(cube, CubeSideType.FRONT);
+
+        //then
+        assertThrows(IllegalStateException.class, () -> boardCube.getCubeSideImageBySide(CubeSideType.BOTTOM));
+    }
+
+    @Test
+    void correctCubeSideImageShouldBeReturnedFromGetCubeSideImageBySide() {
+        //given
+        Cube cube = new Cube(List.of(new CubeSide(CubeSideType.FRONT, CubeSideImage.EVEREST), new CubeSide(CubeSideType.BACK, CubeSideImage.ROCKY)));
+        BoardCube boardCube = new BoardCube(cube, CubeSideType.FRONT);
+
+        //when
+        CubeSideImage activeCubeSideImage = boardCube.getCubeSideImageBySide(CubeSideType.BACK);
+
+        //then
+        assertThat(activeCubeSideImage, is(CubeSideImage.ROCKY));
+    }
+
+}
